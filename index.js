202deb59d1c385607ec283b002dc7d413d92efbd
tunnckoCore <mameto_100@mail.bg>
2016-10-24 02:28:25 +0300

    refactor(*): rewritten to use `koa-compose` and `koa-ip-filter`

--- index.js
+++ index.js
@@ -7,6 +7,69 @@
 
 'use strict'
 
-module.exports = function () {
+const convert = require('koa-convert')
+const compose = require('koa-compose')
+const filter = require('koa-ip-filter')
+const extend = require('extend-shallow')
 
+module.exports = function koaBetterRatelimit (opts) {
+  opts.filter = opts.filter || '**'
+
+  const ipFilter = convert(filter(opts))
+  let store = opts.store && typeof opts.store === 'object'
+    ? opts.store
+    : {}
+
+  return compose([ipFilter, function (ctx, done) {
+    const headers = extend({
+      remaining: 'X-RateLimit-Remaining',
+      reset: 'X-RateLimit-Reset',
+      limit: 'X-RateLimit-Limit'
+    }, opts.headers)
+
+    opts = extend({
+      duration: 1000 * 60 * 60 * 24,
+      limited: 'Too Many Requests',
+      max: 500
+    }, opts)
+
+    const id = ctx.identifier
+    const data = store[id]
+    const now = Date.now()
+    const reset = now + opts.duration
+    const needReset = data && data.remaining === false && data.reset < now
+
+    if (!store.hasOwnProperty(id) || needReset) {
+      store[id] = {
+        id: id,
+        reset: reset,
+        remaining: opts.max
+      }
+    }
+
+    const entry = store[id]
+    entry.remaining = entry.remaining > 0
+        ? entry.remaining - 1
+        : false
+
+    ctx.set(headers.remaining, entry.remaining || 0)
+    ctx.set(headers.reset, entry.reset)
+    ctx.set(headers.limit, opts.max)
+
+    if (typeof entry.remaining === 'number') {
+      return done()
+    }
+
+    const after = entry.reset - (now / 1000) | 0
+    ctx.set('Retry-After', after)
+
+    ctx.status = 429
+    ctx.body = typeof opts.limited === 'function'
+        ? opts.limited(ctx, entry)
+        : opts.limited
+
+    if (opts.throw) {
+      ctx.throw(ctx.status, ctx.body)
+    }
+  }])
 }
--- test.js
+++ test.js
@@ -8,3 +8,31 @@
 /* jshint asi:true */
 
 'use strict'
+
+const ratelimit = require('./index')
+const Koa = require('koa2')
+const app = new Koa()
+
+app
+  .use(function * (next) {
+    try {
+      yield * next
+    } catch (e) {
+      console.log('err', e)
+    }
+  })
+  .use(ratelimit({
+    // it can be double star - globstar
+    // or *.*.*.*
+    id: function () {
+      return '0.0.0.0'
+    },
+    duration: 1000 * 20,
+    // filter: '*.*.*.*',
+    max: 2,
+    throw: true
+  }))
+
+// .listen(2222, function () {
+//   console.log('Foo bar baz!')
+// })
